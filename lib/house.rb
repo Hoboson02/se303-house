cebf9a08477ef18626ee9616ca2c5b06d580ac9b
Ryan Earl <ryanleoearl@gmail.com>
2022-04-12 11:46:49 -0700

    Completion of third test

--- lib/house.rb
+++ lib/house.rb
@@ -3,8 +3,10 @@ class House
 	def line(number)
 		if (number == 1)
 			"This is the house that Jack built.\n"
-		elsif
+		elsif (number == 2)
 			"This is the malt that lay in the house that Jack built.\n"
+		elsif (number == 3)
+			"This is the rat that ate the malt that lay in the house that Jack built.\n"
 		end
 	end
 
