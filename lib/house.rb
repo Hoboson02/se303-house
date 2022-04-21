e462f1119bb80ce0eb1cd6e0806935771927f020
Ryan Earl <ryanleoearl@gmail.com>
2022-04-20 22:18:08 -0700

    Completion of fifth test

--- lib/house.rb
+++ lib/house.rb
@@ -9,6 +9,8 @@ class House
 			"This is the rat that ate the malt that lay in the house that Jack built.\n"
 		elsif (number == 4)
 			"This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+		elsif (number == 5)
+			"This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		end
 	end
 
