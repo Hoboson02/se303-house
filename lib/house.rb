22f13d3cac59a8f46e11a72cd572570e76c9fdde
Ryan Earl <ryanleoearl@gmail.com>
2022-05-04 22:12:37 -0700

    Refactored case 6 and else to be the same.

--- lib/house.rb
+++ lib/house.rb
@@ -13,9 +13,9 @@ class House
 		when 5
 			"This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 6
-			"This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+			"This is the #{horse_hound(number)}#{corn(number)}#{rooster(number)}#{priest(number)}#{tattered(number)}#{maiden(number)}cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		else
-			"This is the #{horse_hound(number)}#{corn(number)}#{rooster(number)}#{priest(number)}#{tattered(number)}maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+			"This is the #{horse_hound(number)}#{corn(number)}#{rooster(number)}#{priest(number)}#{tattered(number)}#{maiden(number)}cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		end
 	end
 
@@ -62,6 +62,14 @@ class House
 	def tattered(number)
 		if number >= 8
 			"man all tattered and torn that kissed the "
+		else
+			""
+		end
+	end
+
+	def maiden(number)
+		if number >= 7
+			"maiden all forlorn that milked the "
 		else
 			""
 		end
