cee2fe2bef3ac5827438d440985e062cce94622d
Ryan Earl <ryanleoearl@gmail.com>
2022-06-04 16:06:24 -0700

    Deleted copied code from House, instantiatied OriginalHouse and called it in the line in House

--- lib/house.rb
+++ lib/house.rb
@@ -1,41 +1,7 @@
 class House
-	def prefix
-		"This is "
-	end
 	def line(number)
-		lines = number.downto(0).collect {|i| phrase(i)}.join()
-		"#{prefix}the #{lines}house that Jack built.\n"
+		OriginalHouse.new(number).line(number)
 	end
-
-	def phrase(number)
-		case number 
-		when 12
-			"horse and the hound and the horn that belonged to the "
-		when 11
-			"farmer sowing his corn that kept the "
-		when 10
-			"rooster that crowed in the morn that woke the "
-		when 9
-			"priest all shaven and shorn that married the "
-		when 8
-			"man all tattered and torn that kissed the "
-		when 7
-			"maiden all forlorn that milked the "
-		when 6
-			"cow with the crumpled horn that tossed the "
-		when 5
-			"dog that worried the "
-		when 4
-			"cat that killed the "
-		when 3
-			"rat that ate the "
-		when 2
-			"malt that lay in the "
-		else
-			""
-		end
-	end
-
 	def recite()
 		1.upto(12).collect {|i| line(i)}.join("\n")
 	end
@@ -43,6 +9,13 @@ class House
 end
 
 class OriginalHouse < House
+
+	attr_reader :number
+	
+	def initialize(number)
+		@number = number 
+	end
+
 	def prefix
 		"This is "
 	end
