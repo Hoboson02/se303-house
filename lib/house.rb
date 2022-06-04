234c7142b6e1607f6dbfab953862a1fc099a95e5
Ryan Earl <ryanleoearl@gmail.com>
2022-06-04 16:00:57 -0700

    copied contents of the House class to OriginalHouse

--- lib/house.rb
+++ lib/house.rb
@@ -42,6 +42,41 @@ class House
 
 end
 
-class OriginalHouse
+class OriginalHouse < House
+	def prefix
+		"This is "
+	end
+	def line(number)
+		lines = number.downto(0).collect {|i| phrase(i)}.join()
+		"#{prefix}the #{lines}house that Jack built.\n"
+	end
 
+	def phrase(number)
+		case number 
+		when 12
+			"horse and the hound and the horn that belonged to the "
+		when 11
+			"farmer sowing his corn that kept the "
+		when 10
+			"rooster that crowed in the morn that woke the "
+		when 9
+			"priest all shaven and shorn that married the "
+		when 8
+			"man all tattered and torn that kissed the "
+		when 7
+			"maiden all forlorn that milked the "
+		when 6
+			"cow with the crumpled horn that tossed the "
+		when 5
+			"dog that worried the "
+		when 4
+			"cat that killed the "
+		when 3
+			"rat that ate the "
+		when 2
+			"malt that lay in the "
+		else
+			""
+		end
+	end
 end
